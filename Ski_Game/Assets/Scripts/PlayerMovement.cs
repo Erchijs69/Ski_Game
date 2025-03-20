8cfbe4289c8de3706706c92dcea93f43337a5a4b
Erchijs69 <146325102+Erchijs69@users.noreply.github.com>
2025-03-20 15:39:27 +0200

    Latest

--- Ski_Game/Assets/Scripts/PlayerMovement.cs
+++ Ski_Game/Assets/Scripts/PlayerMovement.cs
@@ -4,9 +4,9 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField] private float acceleration = 100, turnSpeed = 100, minSpeed = 0, maxSpeed = 1000;
+    [SerializeField] private float acceleration = 100, turnSpeed = 100, minSpeed = 0, maxSpeed = 1000, minAcceleration = 200, maxAcceleration = 1000;
     [SerializeField] private KeyCode leftInput, rightInput;
-    [SerializeField] private LayerMask groundlayers;
+    [SerializeField] private LayerMask groundLayers;
     [SerializeField] private Transform groundTransform;
     
     private float speed = 0;
@@ -20,22 +20,37 @@ public class PlayerMovement : MonoBehaviour
 
     void FixedUpdate()
     {
+        float angle = Mathf.Abs(transform.eulerAngles.y - 180);
+        acceleration = Remap(0, 90, maxAcceleration, minAcceleration, angle);
         speed += acceleration * Time.deltaTime;
         speed = Mathf.Clamp(speed, minSpeed, maxSpeed);
         Vector3 velocity = transform.forward * speed * Time.fixedDeltaTime;
         rb.velocity = new Vector3(velocity.x, rb.velocity.y, velocity.z);
     }
 
+    private float Remap(float oldMin, float oldmax, float newMin, float newMax, float oldValue)
+    {
+        float oldRange = (oldmax - oldMin);
+        float newRange = (newMax - newMin);
+        float newValue = (((oldValue - oldMin) / oldRange) * newRange + newMin);
+        return newValue;
+    }
+
     void Update()
     {
-
-        if(Input.GetKey(leftInput) && transform.eulerAngles.y < 269)
+        bool isGrounded= Physics.Linecast(transform.position, groundTransform.position, groundLayers);
+        if(isGrounded)
         {
-            transform.Rotate(new Vector3(0, turnSpeed * Time.deltaTime, 0), Space.Self);
-        }    
-        if(Input.GetKey(rightInput) && transform.eulerAngles.y > 91) 
-        {
-            transform.Rotate(new Vector3(0, -turnSpeed * Time.deltaTime, 0), Space.Self);
+            if(Input.GetKey(leftInput) && transform.eulerAngles.y < 269)
+            {
+                transform.Rotate(new Vector3(0, turnSpeed * Time.deltaTime, 0), Space.Self);
+            }    
+            if(Input.GetKey(rightInput) && transform.eulerAngles.y > 91) 
+            {
+                transform.Rotate(new Vector3(0, -turnSpeed * Time.deltaTime, 0), Space.Self);
+            }
         }
     }
+
+    
 }
